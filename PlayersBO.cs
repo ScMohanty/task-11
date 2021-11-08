f847a4e8ee8a1cd3d0313f67df57d6584a2540c5
ScMohanty <92923028+ScMohanty@users.noreply.github.com>
2021-11-08 09:01:35 +0530

    Add files via upload

---
+++ PlayersBO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+    class PlayersBO :Players, IComparable
+    {
+
+        public int CompareTo(object obj)
+        {
+            PlayersBO p = (PlayersBO)obj;
+            return this.BattingStyle.CompareTo(p.BattingStyle);
+
+        }
+        public static void SearchPlayerMethod(object[]arr,string name)
+        {
+             );
+
+
+        }
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+  public  class Team
+    {
+     string teamName;
+     string country;
+
+        public string TeamName { get => teamName; set => teamName = value; }
+        public string Country { get => country; set => country = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
